8b847a006240b7df0f1187c712023431c01913e2
yadavjitu06 <120108392+yadavjitu06@users.noreply.github.com>
2025-04-04 16:45:05 +0530

    integrated daisyui plugin with night theme in our app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
-   
-    
-      <>
-    <div class="bg-red-400">new application </div>
-      </>
-  )
+    <>
+      <div class="p-10 dark:p-20">
+        I will have 10 padding on winter theme and 20 padding on night theme
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
